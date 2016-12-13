a4cb1869afb0f9f1c715b23bf95c223aaa27b936
milant88 <milan.tomic.rs@gmail.com>
2016-12-13 22:09:40 +0100

    Inserting tabs - example

--- js/app.js
+++ js/app.js
@@ -8,15 +8,36 @@
     var gems = [
         {
         name: 'Dodecahedron',
-        price: 2.95,
+        price: 2,
         description: '. . .',
         canPurchase: true,
+        images: [
+            {
+                full: '../images/diamond.png'
+            }            
+        ]
     },
     {
        name: 'Pentagonal Gem',
         price: 5.95,
         description: '. . .',
         canPurchase: false,
+        images: [
+            {
+                full: '../images/ruby.png'
+            }            
+        ]
     }
     ];
+    
+    app.controller('PanelController', function() {
+       this.tab = 1; 
+        
+        this.selectTab = function(setTab) {
+          this.tab = setTab;  
+        };
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;  
+        };
+    });
 })();
